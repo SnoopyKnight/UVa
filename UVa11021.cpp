fbab886d8907ff43d9218627fdc5b5f63040e3f0
SnoopyKnight <thedarkknight01@icloud.com>
2017-08-13 09:27:10 +0800

    Add files via upload

---
+++ UVa11021.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+using namespace std;
+
+const int maxn = 1000 + 10;
+const int maxm = 1000 + 10;
+int n,k,m;
+double P[maxn],f[maxm];
+
+int main() {
+	int T;
+	scanf("%d",&T);
+	for(int kase = 1; kase <= T; kase++){
+		scanf("%d%d%d",&n,&k,&m);
+		for(int i=0; i<n; i++)	scanf("%lf",&P[i]);
+		f[0] = 0; f[1] = P[0];
+		for(int i = 0; i < n ; i++){
+
+		}
+	}
+}
